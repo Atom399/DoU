31acf30ba9b1d304c9621ffb46b449598e04e86a
Atom399 <vishuvishal8474@gmail.com>
2021-08-10 12:29:37 +0530

    DoU

--- lib/Pages/Chat/models/user_model.dart
+++ lib/Pages/Chat/models/user_model.dart
@@ -1,11 +1,11 @@
-// class User {
-//   final int? id;
-//   final String? name;
-//   final String? imageUrl;
+class User {
+  final int? id;
+  final String? name;
+  final String? imageUrl;
 
-//   User({
-//     this.id,
-//     this.name,
-//     this.imageUrl,
-//   });
-// }
+  User({
+    this.id,
+    this.name,
+    this.imageUrl,
+  });
+}
